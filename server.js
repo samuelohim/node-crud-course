cdc8736e9063efa375d830f5e0d413159e0ce0ee
Samuel Y <samuel.y@weimeit.com>
2017-01-20 13:15:02 +0800

    controller, routers are in place

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+app = express();
+port = process.env.PORT || 8080;
+
+
+
+app.use(require('./app/routes'));
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+	console.log(`Listening on port ${port}`);
+});
